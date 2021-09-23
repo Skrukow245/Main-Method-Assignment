a75f71f453c765eba1bb333511d14b392d389558
Skrukow245 <skrukow245@gmail.com>
2021-09-23 10:29:27 -0600

    Add project files.

---
+++ Main Method Assignment/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+
+public class Operation
+{
+    public int Add(int data, int value)
+    {
+        return data + value;
+    }
+    public double Add(double data, double value)
+    {
+        return data + value;
+    }
+    internal object Add(string v1, int v2)
+    {
+        return v1 + v2;
+    }
+}
+
---
+++ Main Method Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //var operatorObject = new Operation();
+        //var result = operatorObject.Add(5, 4);                        //pulls from first method
+        //Console.WriteLine(result);
+        //Console.ReadLine();
+
+        //var operatorObject = new Operation();
+        //var result = operatorObject.Add(.5, .9);                      //pulls from second method
+        //Console.WriteLine(result);
+        //Console.ReadLine();
+
+        var operatorObject = new Operation();
+        var result = operatorObject.Add(v1:"one", 2);                        //pull from third method
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
+
